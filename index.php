095d204935fa2cea192920181ec336ec4826c1a1
emilie-ponce <emilie.ponce@laplateforme.io>
2025-02-11 10:57:43 +0100

    création de la page d'accueil avec le header et l'image en fond

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Tektur:wght@400..900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<!--HAUT DE PAGE-->
+    <header>
+        <nav>
+            <ul class="hautdepage">
+                <p class="titrejeu">Quiznight</p>
+                <li><a class="lien" href="index.php">Accueil</a></li>
+                <li><a class="lien" href="">Connexion</a></li>
+            </ul>
+        </nav>
+    </header>
+
+</body>
+</html>
