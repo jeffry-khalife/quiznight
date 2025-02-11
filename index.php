d1b82decc21268b2fecd8efdc0e5a838b8200bf4
emilie-ponce <emilie.ponce@laplateforme.io>
2025-02-11 15:25:59 +0100

    page d'accueil terminée

--- index.php
+++ index.php
@@ -23,5 +23,129 @@
         </nav>
     </header>
 
+<!--PAGE-->
+    <section>
+        <!--quiz friends-->
+        <div class="quiz2">
+            <div class="quizserie">
+                <p class="titrequiz">
+                    QUIZ 1
+                </p>
+                <a href="séries">
+                    <img class="séries2" src="quiznight/images/friends.png" alt="photo série friends">
+                </a>
+                <p class="titresérie">
+                    Friends
+                </p>    
+            </div>
+        </div>
+
+        <!--quiz la casa de papel-->
+        <div class="quiz2">
+            <div class="quizserie">
+                <p class="titrequiz">
+                    QUIZ 2
+                </p>
+                <a href="séries">
+                    <img class="séries2" src="quiznight/images/lacasadepapel.png" alt="photo série lacasadepapel">
+                </a>
+                <p class="titresérie">
+                    La casa de papel
+                </p> 
+            </div>
+        </div>
+
+        <!--quiz arcane-->
+        <div class="quiz2">
+            <div class="quizserie">
+                <p class="titrequiz">
+                    QUIZ 3
+                </p>
+                <a href="séries">
+                    <img class="séries2" src="quiznight/images/arcane.png" alt="photo série arcane">
+                </a>
+                <p class="titresérie">
+                    Arcane
+                </p> 
+            </div>
+        </div>
+
+        <!--quiz prison break-->
+        <div class="quiz2">
+            <div class="quizserie">
+                <p class="titrequiz">
+                    QUIZ 4
+                </p>
+                <a href="séries">
+                    <img class="séries2" src="quiznight/images/prisonbreak.png" alt="photo série prison break">
+                </a>
+                <p class="titresérie">
+                    Prison Break
+                </p> 
+            </div>
+        </div>
+    </section>
+
+    <section class="quizcrcc">
+        <!--quiz chats-->
+        <div class="quiz3">
+            <div class="quizanimal">
+                <p class="titrequiz">
+                    QUIZ 5
+                </p>
+                <a href="">
+                    <img class="animal2" src="quiznight/images/chat.png" alt="photo quiz chats">
+                </a>
+                <p class="titreanimal">
+                    Chats
+                </p>
+            </div>
+        </div>
+
+        <!--quiz requin-->
+        <div class="quiz3">
+            <div class="quizanimal">
+                <p class="titrequiz">
+                    QUIZ 6
+                </p>
+                <a href="">
+                    <img class="animal2" src="quiznight/images/requin.png" alt="photo quiz  requins">
+                </a>
+                <p class="titreanimal">
+                    Requins
+                </p>
+            </div>
+        </div>
+
+        <!--quiz calopsitte-->
+        <div class="quiz3">
+            <div class="quizanimal">
+                <p class="titrequiz">
+                    QUIZ 7
+                </p>
+                <a href="">
+                    <img class="animal2" src="quiznight/images/calopsitte.png" alt="photo quiz calopsitte">
+                </a>
+                <p class="titreanimal">
+                    Calopsitte
+                </p>
+            </div>
+        </div>
+
+        <!--quiz chien-->
+        <div class="quiz3">
+            <div class="quizanimal">
+                <p class="titrequiz">
+                    QUIZ 8
+                </p>
+                <a href="">
+                    <img class="animal2" src="quiznight/images/chien.png" alt="photo quiz chiens">
+                </a>
+                <p class="titreanimal">
+                    Chiens
+                </p>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
