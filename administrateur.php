c98380413f266684baf3c4707c5cc31ac436cd65
emilie-ponce <emilie.ponce@laplateforme.io>
2025-02-13 10:16:35 +0100

    ajout du php pour gérer la gestion du quiz

--- administrateur.php
+++ administrateur.php
@@ -48,22 +48,8 @@
     $database = new QuizNight("localhost", "quiznight", "root", "");
     $quizManager = new Quiz($database);
 
-    //traitement du formulaire
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["ajouterQuizEtQuestion"])){
-        $titre = $_POST["titre"];
-        $description = $_POST["description"];
-        $date_creation = $_POST["date_creation"];
-        $texte_question = $_POST["texte_question"];
-
-        if (!empty($titre) && !empty($description) && !empty($date_creation) && !empty($texte_question)){
-            $id_quiz = $quizManager->ajouterQuiz($titre, $description, $date_creation);
-            $quizManager->ajouterQuestion($id_quiz, $texte_question);
-            echo "Quiz et question ajoutés avec succès !";
-        } else {
-            echo "Veuillez remplir tous les champs.";
-        }
-    }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -71,13 +57,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page administrateur</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Tektur:wght@400..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="administrateur.css">
 </head>
 <body>
-    
-    <h1>Gestion du Quiz</h1>
 
-    <h2>Ajouter un Quiz</h2>
+<!--HAUT DE PAGE-->
+    <header>
+        <nav>
+            <ul class="hautdepage">
+                <p class="titrejeu">Quiznight</p>
+                <li><a class="lien" href="index.php">Accueil</a></li>
+                <li><a class="lien" href="">Connexion</a></li>
+            </ul>
+        </nav>
+    </header>
+
+<!--PAGE-->
+    <h1 class="quizadmin">Gestion du Quiz</h1>
+
+    <h5 class="quizadmin">Ajouter un Quiz</h5>
     <form method="post">
         <label for="titre">Titre:</label>
         <input type="text" id="titre" name="titre" required><br><br>
@@ -88,12 +89,39 @@
         <label for="date_creation">Date de création:</label>
         <input type="date" id="date_creation" name="date_creation" required><br><br>
         
-        <h3>Ajouter une Question</h3>
+        <h4>Ajouter une Question</h5>
         <label for="texte_question">Question:</label>
         <input type="text" id="texte_question" name="texte_question" required><br><br>
         
-        <button type="submit" name="ajouterQuizEtQuestion">Ajouter</button>
+        <label for="texte_question">Question:</label>
+        <input type="text" id="texte_question" name="texte_question" required><br><br>
+
+        <label for="texte_question">Question:</label>
+        <input type="text" id="texte_question" name="texte_question" required><br><br>
+        
+        <button type="submit" name="ajouterQuizEtQuestion">Ajouter</button><br><br>
+        
+        <?php
+        //traitement du formulaire
+            if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["ajouterQuizEtQuestion"])){
+                $titre = $_POST["titre"];
+                $description = $_POST["description"];
+                $date_creation = $_POST["date_creation"];
+                $texte_question = $_POST["texte_question"];
+
+                if (!empty($titre) && !empty($description) && !empty($date_creation) && !empty($texte_question)){
+                    $id_quiz = $quizManager->ajouterQuiz($titre, $description, $date_creation);
+                    $quizManager->ajouterQuestion($id_quiz, $texte_question);
+                    echo "<p id='messageerreur'>Quiz et question ajoutés avec succès !</p>";
+                } else {
+                    echo "<p id='messageerreur'>Veuillez remplir tous les champs.</p>";
+                }
+            }
+        ?>
     </form>
+
+
 
 </body>
 </html>
+
