2b843a464630bc4af74389918486daaf9996734a
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-02-14 13:10:37 +0100

    Ajout du style pour la police d'écriture

--- traiter_reponses.php
+++ traiter_reponses.php
@@ -34,6 +34,7 @@ echo "<!DOCTYPE html>
     <title>Résultats du Quiz - " . $quiz->getTitle() . "</title>
     <link rel='stylesheet' href='style1.css'>
     <link rel='stylesheet' href='style2.css'>
+        <link href='https://fonts.googleapis.com/css2?family=Audiowide&family=Tektur:wght@400..900&display=swap' rel='stylesheet'>
 
 </head>
 
