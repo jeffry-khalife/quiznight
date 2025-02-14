8fe978fa0f2ccfbb04aab7b00f563cb67ff571bc
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-02-14 10:48:55 +0100

    Rectification de la page style utilisé

--- login.php
+++ login.php
@@ -35,14 +35,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>Connexion</title>
-    <link rel='stylesheet' href='style.css'>
-    <link rel='stylesheet' href='nav.css'>
+    <link rel='stylesheet' href='style1.css'>
+    <link rel='stylesheet' href='style2.css'>
 </head>
+
+<header>
+        <nav>
+            <ul class="hautdepage">
+                <p class="titrejeu"></p>
+                <li><a class="lien" href="index.php">QuizNight</a></li>
+                <li><a class="lien" href="login.php">Connexion</a></li>
+            </ul>
+        </nav>
+    </header>
 
 <body>
 
 </body>
-</html>
     <div class="app">
         <div class="quiz">
             <h3>Connexion</h3>
@@ -57,3 +66,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </form>
         </div>
     </div>
+
+    <footer>
+        <div class="réseauxsociaux">
+            <img class="réseaux" src="img/instagram.png" alt="photo logo instagram">
+            <img class="réseaux" src="img/twitter.png" alt="photo logo twitter">
+            <img class="réseaux" src="img/tik-tok.png" alt="photo logo tiktok">
+        </div>
+    </footer>
+</html>
